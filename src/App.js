97565a325593268fdcaf531e5eacac6eea52c259
unknown <mohamedhelalilyes@gmail.com>
2022-11-03 12:50:05 +0100

    Created the FeedbackStats component to calculte the total number of feedbacks and the average rating (using some high level array functions)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import FeedbackItemList from "./components/FeedbackItemList";
+import FeedbackStats from "./components/FeedbackStats";
 import Header from "./components/Header";
 
 function App() {
   const data = [
     { itemId: 55589, itemRating: 8, itemText: "Lorem ipulsum" },
     { itemId: 5599959, itemRating: 6, itemText: "Lorem ipulsum 22" },
-    { itemId: 5115589, itemRating: 10, itemText: "Lorem ipuhhhlsum" },
+    { itemId: 5115589, itemRating: 5, itemText: "Lorem ipuhhhlsum" },
   ];
 
   const [feedBackItems, setFeedbackItems] = useState(data);
@@ -26,6 +27,7 @@ function App() {
         desc="a React application to make CRUD of feedbacks"
       />
       <div className="container">
+        <FeedbackStats feedbacks={feedBackItems} />
         <FeedbackItemList
           feedbacks={feedBackItems}
           deleteFeedbacks={deleteFeedbacks}
---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,26 @@
+function FeedbackStats({ feedbacks }) {
+  const averageRatings = () => {
+    if (!feedbacks || feedbacks.length === 0) {
+      return 0;
+    }
+
+    return (
+      feedbacks
+        .map((item) => {
+          return item.itemRating;
+        })
+        .reduce((totalRatings, currentRatings) => {
+          return totalRatings + currentRatings;
+        }) / feedbacks.length
+    );
+  };
+
+  return (
+    <div className="feedback-stats">
+      <h4>Total of Ratings: {feedbacks.length}</h4>
+      <h4>Average Rating: {averageRatings().toFixed(2)}</h4>
+    </div>
+  );
+}
+
+export default FeedbackStats;
